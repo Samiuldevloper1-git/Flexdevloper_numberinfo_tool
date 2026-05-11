379ce1cd83969f04ae976dc8d180750f421a429e
Samiuldevloper1-git <Samiuldevloper1@gmail.com>
2026-05-11 12:25:40 +0530

    Add no_data function to display no data message

---
+++ core/nodata.py
@@ -0,0 +1,14 @@
+from colorama import Fore
+
+def no_data():
+
+    print(Fore.RED + "\n╔══════════════════════════════════╗")
+    print(Fore.RED + "║            NO DATA              ║")
+    print(Fore.RED + "╚══════════════════════════════════╝")
+
+    print(Fore.WHITE + "\n[✗] No Leak Data Found")
+    print(Fore.WHITE + "[✗] Number Not Available In Database")
+
+    print(Fore.MAGENTA + "\n━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print(Fore.YELLOW + " Credit : Samiul Devloper")
+    print(Fore.MAGENTA + "━━━━━━━━━━━━━━━━━━━━━━━━━━")
